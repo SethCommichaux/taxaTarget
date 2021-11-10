224ff3d3b1b6cdff485f60e9f6331bcd62044507
Seth.Commichaux <yofi01@gmail.com>
2021-11-10 12:51:01 -0500

    update git repo scripts

--- run_pipeline_scripts/run_protist_pipeline_fda.py
+++ run_pipeline_scripts/run_protist_pipeline_fda.py
@@ -8,50 +8,61 @@ import os
 import sys
 import argparse
 
-# Set paths to directories and databases
-#
-diamond="diamond"
-kaiju="kaijux"
-protist_data="/lustre/scratch/Seth.Commichaux/Busco_Protist_Pipeline/data/"
-run_pipeline="/lustre/scratch/Seth.Commichaux/Busco_Protist_Pipeline/run_pipeline_scripts/"
-kaijuDB=protist_data+"/marker_geneDB.fasta.kaiju.fmi"
-queryDB=protist_data+"/marker_geneDB.fasta"
-
 # Read in commandline arguments
 #
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", help="Input fastq file", required = True)
-parser.add_argument("-p", help="Threshold padding to add for thresholds trained with missing data; default = 0.5",default = '0.5')
+parser.add_argument("-dir", help="Path to taxaTarget directory e.g., /usr/home/taxaTarget", required = True)
+parser.add_argument("-p", help="Padding to add to thresholds trained with missing data. Range from 0 to 1, with 1 filtering results most aggressively. Default = 0.5",default = '0.5')
 parser.add_argument("-t", help="Number of threads to use; default = 12",default = '12')
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 args = parser.parse_args()
 
+# parse commandline arguments
+#
 reads_fastq = args.f
 padding = args.p
 threads = args.t
 out = reads_fastq+'.taxaTarget'
 
+if args.dir.endswith('/'):
+        taxaTarget = args.dir
+else:
+        taxaTarget = args.dir+'/'
+
+# Paths to directories, tools, and databases
+#
+protist_data=taxaTarget+"data/"
+run_pipeline=taxaTarget+"run_pipeline_scripts/"
+kaijuDB=protist_data+"/marker_geneDB.fasta.kaiju.fmi"
+queryDB=protist_data+"/marker_geneDB.fasta"
+kaiju=protist_data+"kaiju/bin/kaijux"
+diamond=protist_data+"diamond"
+
 # Create and change to output directory
 #
 os.system('mkdir '+out)
-os.chdir(out)
 
 # Run kaiju to query fastq reads against protein sequence binning databse (binningDB.fasta)
 #
-os.system('%s -f %s -i ../%s -z %s -m 9 | grep "^C" > kaiju' % (kaiju,kaijuDB,reads_fastq,threads))
+print('Beginning analysis. Mapping reads with Kaiju.')
+os.system('%s -f %s -i ../%s -z %s -m 9 | grep "^C" > %s/kaiju' % (kaiju,kaijuDB,reads_fastq,threads,out))
 
 
 # Extract reads that aligned to binning database
 #
-os.system('python %s/extract_kaiju_reads.py -k kaiju -s ../%s -o kaiju.fasta' % (run_pipeline,reads_fastq))
+print('Extracting reads mapped by Kaiju.')
+os.system('python %s/extract_kaiju_reads.py -k %s/kaiju -s %s -o %s/kaiju.fasta' % (run_pipeline,out,reads_fastq,out))
 
 # Align binned reads, with Diamond, to queryDB
 #
 # --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq qlen
-os.system('%s blastx --sensitive --min-score 55 --db %s --query kaiju.fasta --threads %s --outfmt 6 --out kaiju.fasta.diamond' % (diamond,queryDB,threads))
+print('Aligning reads with Diamond.')
+os.system('%s blastx --sensitive --min-score 55 --db %s --query %s/kaiju.fasta --threads %s --outfmt 6 --out %s/kaiju.fasta.diamond' % (diamond,queryDB,out,threads,out))
 
 # Classify reads
 #
-os.system('python %s/classify_reads_strict.py -d kaiju.fasta.diamond -dir %s/ -p %s' % (run_pipeline,protist_data,padding))
+os.system('python %s/classify_reads_strict.py -d %s/kaiju.fasta.diamond -dir %s/ -p %s' % (run_pipeline,out,protist_data,padding))
+
